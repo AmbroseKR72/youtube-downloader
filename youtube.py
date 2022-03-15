a746f7fa9af6c1bd713b26119583727e826732d9
AmbroseKR72 <98053352+AmbroseKR72@users.noreply.github.com>
2022-03-15 19:55:43 +0300

    Add files via upload

---
+++ youtube.py
@@ -0,0 +1,63 @@
+from tkinter import *
+from tkinter import filedialog
+from moviepy import*
+from moviepy.editor import VideoFileClip
+from pytube import YouTube
+
+import shutil
+
+
+
+print()
+#Functions
+def select_path():
+    #allows user to select path from the explorer
+    path = filedialog.askdirectory()
+    path_label.config(text= path)
+    screen.title('Downloading...')
+
+def download_file():
+    #get user path
+    get_link = link_field.get()
+    #get selected path
+    user_path= path_label.cget("text")
+    #Download the video
+    mp4_video=YouTube(get_link).streams.get_highest_resolution().download()
+    vid_clip=VideoFileClip(mp4_video)
+    vid_clip.close()
+    #move to selected directory
+    shutil.move(mp4_video,user_path)
+    screen.title('Download complete!')
+
+
+
+screen = Tk()
+title = screen.title('Youtube Downloader')
+canvas = Canvas(screen, width =500, height = 500)
+canvas.pack()
+
+#logo
+logo_img = PhotoImage(file ='yt_image.png')
+#resizing  logo image
+logo_img = logo_img.subsample(3,3)
+canvas.create_image(250,80,image=logo_img)
+#link field
+link_field = Entry(screen, width =50)
+link_label = Label(screen, text = "Enter Download link:", font = ('Arial',15))
+#Select path for saving the file
+path_label =Label(screen, text = "Select path for Download", font =('Arial', 15))
+select_btn = Button(screen,text ="Select",command=select_path)
+#Add to window
+canvas.create_window(250,280,window=path_label)
+canvas.create_window(250,330,window=select_btn)
+
+#Add widgets to window
+canvas.create_window(250,170, window = link_label)
+canvas.create_window(250,220, window = link_field)
+#Download btns
+download_btn = Button(screen, text = "Download File", command=download_file)
+#Add to canvas
+canvas.create_window(250,390,window = download_btn)
+
+
+screen.mainloop()
